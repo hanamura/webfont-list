2508afedd0117d5ff5a1e38eb59e6e5eb84034f5
Taro Hanamura <m@hanamurataro.com>
2015-02-11 18:49:49 +0900

    Add main file

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  typesquare:     function() { return require('./script/typesquare'); },
+  typesquareData: function() { return require('./data/typesquare'); },
+  fontplus:       function() { return require('./script/fontplus'); },
+  fontplusData:   function() { return require('./data/fontplus'); },
+};
